0a2583ecfb4b7f16b5ab7af7d3095244edd5d4e0
RaviThakor <ravit@zignuts.com>
2023-01-23 19:48:56 +0530

    day end commit

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -9,13 +9,20 @@ class DemoController extends Controller
 {
     //In This Controller I have Perform Query Builder method to perform crud and databse Querys.
     public function ReadQueryBuilder(){
-        //This Will Create Instance of Databse table and allow as to perform operation
+        //This Will Create Instance of Databse table and allow us to perform operation
         //$student = DB::table('students');
+
         //get data from Database table using Query Builder
         //$students = DB::table('students')->get();//this will simply run SELECT * From students;
         //return view('display',compact('students'));
-        $students = DB::table('students')->first();//This Will Give only First record from the data base table
+
+        //$students = DB::table('students')->first();//This Will Give only First record from the data base table
+
+        //where() and find()[is for primary key]  Method is use to find the coloumn
+        //$students = DB::table('students')->where('sem','4')->first();
+        $students = DB::table('students')->where('sem','4')->get();
         dd($students);
+        //return $students;
 
     }
 
